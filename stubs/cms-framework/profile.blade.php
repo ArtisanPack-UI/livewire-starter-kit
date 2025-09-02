a976b133c1b9c7053265083e087d241de4ff483d
Jacob Martella <me@jacobmartella.com>
2025-09-02 17:17:38 -0500

    fixed a few issues with the starter kit when using the cms-framework package

--- stubs/cms-framework/profile.blade.php
+++ stubs/cms-framework/profile.blade.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\User;
+use ArtisanPackUI\CMSFramework\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Validation\Rule;
